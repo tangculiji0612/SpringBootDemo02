50f001a11b48f76d90d7f72fb1cf7412a551e63b
tangculiji <451710238@qq.com>
2020-09-21 22:22:03 +0800

    第四次提交

---
+++ SpringBootDemo02/src/main/java/com/imti/model/StudentInfo.java
@@ -0,0 +1,16 @@
+package com.imti.model;
+/**@文件名: StudentInfo.java
+ * @类功能说明: 
+ * @作者: LiJi
+ * @Email: 451710238@qq.com
+ * @日期: 2020年9月21日下午10:21:38
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: LiJi</li> 
+ * 	 <li>日期: 2020年9月21日下午10:21:38</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class StudentInfo {
+
+}
