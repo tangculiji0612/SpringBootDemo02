30dbbd9cf6dc88274fc24fbb45782675e6859953
tangculiji <451710238@qq.com>
2020-09-21 22:12:37 +0800

    第二次提交

---
+++ SpringBootDemo02/src/main/java/com/imti/SpringBootApplication.java
@@ -0,0 +1,23 @@
+package com.imti;
+
+import org.springframework.boot.SpringApplication;
+
+/**@文件名: SpringBootApplication.java
+ * @类功能说明: 
+ * @作者: LiJi
+ * @Email: 451710238@qq.com
+ * @日期: 2020年9月21日下午10:03:29
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: LiJi</li> 
+ * 	 <li>日期: 2020年9月21日下午10:03:29</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+}
---
+++ SpringBootDemo02/src/main/java/com/imti/controller/Hello.java
@@ -0,0 +1,28 @@
+package com.imti.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**@文件名: Hello.java
+ * @类功能说明: 
+ * @作者: LiJi
+ * @Email: 451710238@qq.com
+ * @日期: 2020年9月21日下午10:05:01
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: LiJi</li> 
+ * 	 <li>日期: 2020年9月21日下午10:05:01</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+@Controller
+public class Hello {
+	
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "SpringBoot你大爷的！";
+	}
+
+}
